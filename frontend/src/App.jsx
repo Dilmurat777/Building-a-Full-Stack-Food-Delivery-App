1324ab9b7192758d34086d2e176ae01c3e77e41a
user.email <mrdilmurat777@gmail.com>
2025-07-28 23:36:47 +0600

    feat: add comp. Navbar, Header, ExploreMenu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar/Navbar"
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import PLaceOrder from './pages/PlaceOrder/PLaceOrder';
 
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<PLaceOrder />} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/PlaceOrder/PLaceOrder.jsx
@@ -0,0 +1,11 @@
+import './PlaceOrder.css'
+
+const PLaceOrder = () => {
+  return (
+	<div>
+	  
+	</div>
+  )
+}
+
+export default PLaceOrder
